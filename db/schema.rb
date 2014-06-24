9577b42911deb101a64e5336d4871f8b94db91dd
Stephanie Lauziere <salauzie@gmail.com>
2014-06-24 14:00:41 -0400

    add pins/users association

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623193050) do
+ActiveRecord::Schema.define(version: 20140624171936) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
 end
